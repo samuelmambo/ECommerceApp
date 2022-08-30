d6f778aa732e9ca885b3c282c952f4463488cf81
samuelmambo <museemukuu@gmail.com>
2022-08-30 14:15:22 +0300

    Home activity

--- app/src/main/java/com/example/ecommerceapp/SplashActivity.kt
+++ app/src/main/java/com/example/ecommerceapp/SplashActivity.kt
@@ -1,11 +1,25 @@
 package com.example.ecommerceapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SplashActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
+        //Button with intent
+
+        val btn = findViewById<Button>(R.id.btn_intent)
+
+
+        btn.setOnClickListener {
+           val try_intent = Intent(this,HomeActivity::class.java)
+            startActivity(try_intent)
+        }
     }
 }
