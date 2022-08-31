71080a82539e8c5704758e5cfc309f6871a91a24
samuelmambo <museemukuu@gmail.com>
2022-08-31 13:45:58 +0300

    first intent

--- app/src/main/java/com/example/ecommerceapp/HomeActivity.kt
+++ app/src/main/java/com/example/ecommerceapp/HomeActivity.kt
@@ -1,11 +1,19 @@
 package com.example.ecommerceapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btn_intent2 = findViewById<Button>(R.id.btn_intent2)
+        btn_intent2.setOnClickListener {
+            val button_intent2 = Intent(this, SplashActivity::class.java)
+            startActivity(button_intent2)
+        }
     }
 }
